dde31ffb0aa4dfe75320f0e560b574b5c64e7a3a
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2024-11-15 18:18:33 +0200

    chore: use port and host from config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import { app } from "./app";
+import { config } from "./config";
 
 const start = async () => {
   try {
-    await app.listen({ port: 1234 });
+    await app.listen({
+      host: config.ADDRESS,
+      port: config.PORT,
+    });
     app.log.info("server started");
   } catch (err) {
     app.log.error(err);
