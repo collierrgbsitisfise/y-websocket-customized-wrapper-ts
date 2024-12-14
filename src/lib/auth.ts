d84346bcec09d7ee62a030e052c2f08daa8ece0d
collierrgbsitisfise <vadim.vicolaev.v@gmail.com>
2024-12-14 18:01:35 +0200

    return original 'getUserDataFromJwtWithSignatureVerefication'

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,16 @@
 import jwt from "jsonwebtoken";
 
-// eslint-disable-next-line
 import { config } from "./../config";
 
 export type JwtTokenPayload = {
   userId: string;
   name: string;
 };
-export function getUserDataFromJwtWithSignatureVerification(
-  token: string,
+export function getUserDataFromJwtWithSignatureVerefication(
+  token: string
 ): JwtTokenPayload | null {
   try {
-    // eslint-disable-next-line
-    const decoded = jwt.decode(
-      token /** Buffer.from(config.JWT_SECRET, "base64") */,
-    );
+    const decoded = jwt.verify(token, Buffer.from(config.JWT_SECRET, "base64"));
     return decoded as JwtTokenPayload;
   } catch (error) {
     console.error("Token verification failed:", error);
